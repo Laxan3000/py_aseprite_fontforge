ffe61f2546f451048c4e6571072b5d68594b1d9d
Laxan3000 <alan73097@gmail.com>
2025-12-01 17:11:23 +0100

    Implement changes

---
+++ src/makechar.py
@@ -0,0 +1,70 @@
+from aseprite import AsepriteFile
+
+SQUARE_SIZE = 16
+ASE_NAME = "project.ase"
+SFD_NAME = 'font.sfd'
+FONT_SAVE_NAME = f"font."
+
+def validate(chars) -> int:
+    try: return int(chars)
+    except: return -1
+
+
+print("-- program ready to start --")
+
+with open(ASE_NAME, "rb") as f:
+    ase = AsepriteFile(f.read())
+
+font = fontforge.open(SFD_NAME) # type: ignore
+font.selection.select(glyph := (input('Select the character: ')[0]))
+font.clear()
+c= font.createChar(-1, glyph)
+pen = c.glyphPen()
+
+print("-- contents of gliph erased --")
+
+len_layers = len(ase.layers)
+print(
+    "Select the index of the layer to import:",
+    *[
+        f'{i} - {layer.name}'
+        for i, layer in enumerate(
+            _ for _ in ase.layers if _.layer_type == 0)
+    ],
+    sep='\n'
+)
+while not 0 <= (index := validate(input("- : "))) < len_layers: pass
+
+print("-- value accepted, evaluating changes -- ")
+
+for i, byte in enumerate((chunk := ase.frames[0].chunks[index + len_layers + 2]).data['data']):
+    print('##' if byte else '  ', end='')
+    if (i + 1) % SQUARE_SIZE == 0: print()
+    if not byte: continue
+    x, y = i % SQUARE_SIZE + chunk.x_pos, SQUARE_SIZE - i // SQUARE_SIZE - chunk.y_pos
+    pen.moveTo((x, y))
+    pen.lineTo((x+1, y))
+    pen.lineTo((x+1, y-1))
+    pen.lineTo((x, y-1))
+    pen.closePath()
+
+print("-- adjusting character --")
+
+c.removeOverlap()
+c.simplify()
+c.correctDirection()
+
+print("-- all done! saving changes --")
+
+pen = None
+font.save(SFD_NAME)
+
+if input("would you also like to create the fonts? y/[n]:") == 'y':
+    font.generate(f"{FONT_SAVE_NAME}woff2")
+    font.generate(f"{FONT_SAVE_NAME}woff")
+    font.generate(f"{FONT_SAVE_NAME}ttf")
+    font.generate(f"{FONT_SAVE_NAME}otf")
+    
+    print("-- fonts generated! --")
+
+font.close()
